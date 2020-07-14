f1e648830701a50f323ac605ab22682f8f120cea
Rustam Khasanshin <rustery@gmail.com>
2020-07-14 18:43:17 +0300

    clear code

--- clock.go
+++ clock.go
@@ -13,32 +13,28 @@ type Clock struct {
 
 // New method generates a new clock instance from hours and minutes params
 func New(h, m int) Clock {
-	hours, minutes := generate(h*60 + m)
-	return Clock{hours, minutes}
+	days := float64(h*60+m) / 1440
+	clearDays := math.Floor(days)
+	hours := (days - clearDays) * 24
+	clearHours := math.Floor(hours)
+	minutes := (hours - clearHours) * 60
+	clearMinutes := math.Round(minutes)
+	return Clock{int(clearHours), int(clearMinutes)}
 }
 
+// String method returns string view of a clock instance data
 func (c Clock) String() string {
 	return fmt.Sprintf("%02d:%02d", c.h, c.m)
 }
 
 // Add method adds minutes to clock instance
 func (c Clock) Add(m int) Clock {
-	c.h, c.m = generate(c.h*60 + c.m + m)
+	c = New(c.h, c.m+m)
 	return c
 }
 
 // Subtract method subtracts minutes from clock instance
 func (c Clock) Subtract(m int) Clock {
-	c.h, c.m = generate(c.h*60 + c.m - m)
+	c = New(c.h, c.m-m)
 	return c
-}
-
-func generate(initialMinutes int) (h, m int) {
-	days := float64(initialMinutes) / 1440
-	clearDays := math.Floor(days)
-	hours := (days - clearDays) * 24
-	clearHours := math.Floor(hours)
-	minutes := (hours - clearHours) * 60
-	clearMinutes := math.Round(minutes)
-	return int(clearHours), int(clearMinutes)
 }
