f335c380f5f72e52c3f0d0be2ee82cceec9ada9d
Rustam Khasanshin <rustery@gmail.com>
2020-07-17 11:55:14 +0300

    simplify

--- clock.go
+++ clock.go
@@ -3,38 +3,46 @@ package clock
 
 import (
 	"fmt"
-	"math"
 )
 
 // Clock is a type that stores passed hours and minutes
 type Clock struct {
-	h, m int
+	m int
 }
 
 // New method generates a new clock instance from hours and minutes params
 func New(h, m int) Clock {
-	days := float64(h*60+m) / 1440
-	clearDays := math.Floor(days)
-	hours := (days - clearDays) * 24
-	clearHours := math.Floor(hours)
-	minutes := (hours - clearHours) * 60
-	clearMinutes := math.Round(minutes)
-	return Clock{int(clearHours), int(clearMinutes)}
+	minutes := h*60 + m
+	minutes %= 24 * 60
+	if minutes < 0 {
+		minutes += 24 * 60
+	}
+	return Clock{minutes}
 }
 
 // String method returns string view of a clock instance data
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.h, c.m)
+	return fmt.Sprintf("%02d:%02d", c.hours(), c.minutes())
 }
 
 // Add method adds minutes to clock instance
 func (c Clock) Add(m int) Clock {
-	c = New(c.h, c.m+m)
+	c = New(c.hours(), c.minutes()+m)
 	return c
 }
 
 // Subtract method subtracts minutes from clock instance
 func (c Clock) Subtract(m int) Clock {
-	c = New(c.h, c.m-m)
+	c = New(c.hours(), c.minutes()-m)
 	return c
+}
+
+// Hours method returns hours of clock instance
+func (c Clock) hours() int {
+	return (c.m / 60) - (c.m / (60 * 24))
+}
+
+// Minutes method returns minutes of clock instance
+func (c Clock) minutes() int {
+	return c.m - (c.hours() * 60)
 }
